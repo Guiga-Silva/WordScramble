48725fa250fcdfe340a9fad5939e8f4d56f946ec
Guiga-Silva <guiga.silvaa2@gmail.com>
2023-03-05 09:40:51 -0300

    first commit

---
+++ WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Guilherme Silva on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
